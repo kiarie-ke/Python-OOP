7d2d8a19edc4add0017e083914ba9044d1de3e4a
Kenneth Kiarie <mwangikennethkiarie@gmail.com>
2026-04-13 11:56:37 +0300

    object and clases

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 class Dog:
+    def __init__(self, name, breed, owner):
+        self.name = name
+        self.breed = breed
+        self.owner = owner
+
     def bark(self):
         print("Whoof whoof")
-dog1 = Dog()
-dog1.bark()        
 
+class Owner:
+    def __init__(self, name, address, contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = contact_number
 
-dog2 = Dog()
-dog2.bark()
+owner1 = Owner("Kelvin", "1234", "0758263468")
+dog1 = Dog("Kenneth", "Garman", owner1)
+print(dog1.owner.name)
+
+owner2 = Owner("omollo", "2344", "07992084u")
+dog2 = Dog("Kiash", "brush", owner2)
+print(dog2.owner.name)
